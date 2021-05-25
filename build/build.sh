042532c2acb78eeab1eb5846ae043a9365a94002
goedgecloud <goedgecloud@gmail.com>
2021-05-25 11:05:55 +0800

    调整在Mac OS上的编译脚本

--- build/build.sh
+++ build/build.sh
@@ -48,7 +48,38 @@ function build() {
 	fi
 
 	echo "building ..."
-	env GOOS=${OS} GOARCH=${ARCH} go build -o $DIST/bin/${NAME} -ldflags="-s -w" $ROOT/../cmd/edge-node/main.go
+
+	MUSL_DIR="/usr/local/opt/musl-cross/bin"
+	CC_PATH=""
+	CXX_PATH=""
+	if [[ `uname -a` == *"Darwin"* && "${OS}" == "linux" ]]; then
+		# /usr/local/opt/musl-cross/bin/
+		if [ "${ARCH}" == "amd64" ]; then
+			CC_PATH="x86_64-linux-musl-gcc"
+			CXX_PATH="x86_64-linux-musl-g++"
+		fi
+		if [ "${ARCH}" == "386" ]; then
+			CC_PATH="i486-linux-musl-gcc"
+			CXX_PATH="i486-linux-musl-g++"
+		fi
+		if [ "${ARCH}" == "arm64" ]; then
+			CC_PATH="aarch64-linux-musl-gcc"
+			CXX_PATH="aarch64-linux-musl-g++"
+		fi
+		if [ "${ARCH}" == "mips64" ]; then
+			CC_PATH="mips64-linux-musl-gcc"
+			CXX_PATH="mips64-linux-musl-g++"
+		fi
+		if [ "${ARCH}" == "mips64le" ]; then
+			CC_PATH="mips64el-linux-musl-gcc"
+			CXX_PATH="mips64el-linux-musl-g++"
+		fi
+	fi
+	if [ ! -z $CC_PATH ]; then
+		env CC=$MUSL_DIR/$CC_PATH CXX=$MUSL_DIR/$CXX_PATH GOOS=${OS} GOARCH=${ARCH} CGO_ENABLED=1 go build -o $DIST/bin/${NAME} -ldflags "-linkmode external -extldflags -static" $ROOT/../cmd/edge-node/main.go
+	else
+		env GOOS=${OS} GOARCH=${ARCH} CGO_ENABLED=1 go build -o $DIST/bin/${NAME} -ldflags="-s -w" $ROOT/../cmd/edge-node/main.go
+	fi
 
 	# delete hidden files
 	find $DIST -name ".DS_Store" -delete
