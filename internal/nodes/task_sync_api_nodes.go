dc9aea6c29377f917b0b2cc3224d6c157c5d498c
goedgecloud <goedgecloud@gmail.com>
2021-11-05 15:37:07 +0800

    规范命名

--- internal/nodes/task_sync_api_nodes.go
+++ internal/nodes/task_sync_api_nodes.go
@@ -64,7 +64,7 @@ func (this *SyncAPINodesTask) Loop() error {
 	}
 
 	newEndpoints := []string{}
-	for _, node := range resp.Nodes {
+	for _, node := range resp.ApiNodes {
 		if !node.IsOn {
 			continue
 		}
