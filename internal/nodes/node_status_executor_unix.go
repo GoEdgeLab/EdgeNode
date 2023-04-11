5d53eaba106126af7bda8c8611c6cefbc402c528
goedgecloud <goedgecloud@gmail.com>
2023-04-11 18:51:56 +0800

    优化可用内存检查

--- internal/nodes/node_status_executor_unix.go
+++ internal/nodes/node_status_executor_unix.go
@@ -40,7 +40,7 @@ func (this *NodeStatusExecutor) updateMem(status *nodeconfigs.NodeStatus) {
 		if minFreeMemory > 1<<30 {
 			minFreeMemory = 1 << 30
 		}
-		if stat.Free < minFreeMemory {
+		if stat.Available > 0 && stat.Available < minFreeMemory {
 			runtime.GC()
 			debug.FreeOSMemory()
 		}
