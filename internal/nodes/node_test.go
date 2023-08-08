4e4059ea28aed8b4ad8d8152f5c2f32b85631efd
goedgecloud <goedgecloud@gmail.com>
2023-08-08 11:10:51 +0800

    优化代码

--- internal/nodes/node_test.go
+++ internal/nodes/node_test.go
@@ -1,11 +1,7 @@
 package nodes
 
 import (
-	"github.com/TeaOSLab/EdgeCommon/pkg/rpc/pb"
-	"github.com/golang/protobuf/proto"
 	_ "github.com/iwind/TeaGo/bootstrap"
-	"io"
-	"strconv"
 	"testing"
 )
 
@@ -21,30 +17,4 @@ func TestNode_Test(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Log("ok")
-}
-
-func TestNode_Proto_Buffer(t *testing.T) {
-	buff := proto.NewBuffer([]byte{})
-	for i := 0; i < 10; i++ {
-		err := buff.EncodeMessage(&pb.NodeStreamMessage{
-			RequestId: int64(i),
-			Code:      "msg" + strconv.Itoa(i),
-		})
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	for i := 0; i < 11; i++ {
-		msg := &pb.NodeStreamMessage{}
-		err := buff.DecodeMessage(msg)
-		if err != nil {
-			if err == io.EOF || err == io.ErrUnexpectedEOF {
-				break
-			} else {
-				t.Fatal(err)
-			}
-		}
-		t.Log(msg.Code, msg.RequestId)
-	}
 }
