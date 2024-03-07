796d0244d5e0e8489ec33b501fce8a2a6e1f6a70
goedgecloud <goedgecloud@gmail.com>
2024-03-07 18:46:15 +0800

    build-all.sh不编译amd64和arm64之外的架构

--- build/build-all.sh
+++ build/build-all.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 ./build.sh linux amd64
-./build.sh linux 386
+#./build.sh linux 386
 ./build.sh linux arm64
-./build.sh linux mips64
-./build.sh linux mips64le
-./build.sh darwin amd64
-./build.sh darwin arm64
+#./build.sh linux mips64
+#./build.sh linux mips64le
+#./build.sh darwin amd64
+#./build.sh darwin arm64
