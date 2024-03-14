935a59bbb7599d75d79f5c87fc85c14530140c86
goedgecloud <goedgecloud@gmail.com>
2024-03-14 10:28:12 +0800

    优化测试脚本

--- build/test.sh
+++ build/test.sh
@@ -6,6 +6,9 @@ if [ -z "$TAG" ]; then
 	TAG="community"
 fi
 
+# stop node
+go run -tags=${TAG}  ../cmd/edge-node/main.go stop
+
 # reference: https://pkg.go.dev/cmd/go/internal/test
 go clean -testcache
 go test -timeout 10s -tags="${TAG}" -cover ../...
