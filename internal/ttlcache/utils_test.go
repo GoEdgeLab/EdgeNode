bf762ca8212f230873b6b064ec55e63e0fba5597
goedgecloud <goedgecloud@gmail.com>
2024-04-17 17:18:11 +0800

    增加测试用例

--- internal/ttlcache/utils_test.go
+++ internal/ttlcache/utils_test.go
@@ -1,6 +1,7 @@
 package ttlcache
 
 import (
+	"github.com/cespare/xxhash/v2"
 	"runtime"
 	"testing"
 )
@@ -9,5 +10,12 @@ func BenchmarkHashKey(b *testing.B) {
 	runtime.GOMAXPROCS(1)
 	for i := 0; i < b.N; i++ {
 		HashKey([]byte("HELLO,WORLDHELLO,WORLDHELLO,WORLDHELLO,WORLDHELLO,WORLDHELLO,WORLD"))
+	}
+}
+
+func BenchmarkHashKey2(b *testing.B) {
+	runtime.GOMAXPROCS(1)
+	for i := 0; i < b.N; i++ {
+		xxhash.Sum64String("HELLO,WORLDHELLO,WORLDHELLO,WORLDHELLO,WORLDHELLO,WORLDHELLO,WORLD")
 	}
 }
