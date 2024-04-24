8a6d9aa0296676123585333cc01b49b198cec4ca
goedgecloud <goedgecloud@gmail.com>
2024-04-24 09:03:15 +0800

    优化KV相关日志

--- internal/utils/kvstore/logger.go
+++ internal/utils/kvstore/logger.go
@@ -2,6 +2,11 @@
 
 package kvstore
 
+import (
+	"fmt"
+	"github.com/TeaOSLab/EdgeNode/internal/remotelogs"
+)
+
 type Logger struct {
 }
 
@@ -10,8 +15,13 @@ func NewLogger() *Logger {
 }
 
 func (this *Logger) Infof(format string, args ...any) {
-
+	// stub
 }
-func (this *Logger) Fatalf(format string, args ...any) {
 
+func (this *Logger) Errorf(format string, args ...any) {
+	remotelogs.Error("KV", fmt.Sprintf(format, args...))
+}
+
+func (this *Logger) Fatalf(format string, args ...any) {
+	remotelogs.Error("KV", fmt.Sprintf(format, args...))
 }
