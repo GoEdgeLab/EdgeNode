c4790779a2b6a961afb7ab0d39db658b4a622e13
goedgecloud <goedgecloud@gmail.com>
2024-05-03 11:22:16 +0800

    版本号修改为1.3.8.2

--- internal/const/const.go
+++ internal/const/const.go
@@ -1,14 +1,13 @@
 package teaconst
 
 const (
-	Version = "1.3.8.1"
+	Version = "1.3.8.2"
 
 	ProductName = "Edge Node"
 	ProcessName = "edge-node"
 
 	Role = "node"
 
-	EncryptKey    = "8f983f4d69b83aaa0d74b21a212f6967"
 	EncryptMethod = "aes-256-cfb"
 
 	// SystemdServiceName systemd
